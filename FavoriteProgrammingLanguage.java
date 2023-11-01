65b22c532d4debcd2eebc8305150a80737a0f43e
dwilcox <wilcox.dean93@gmail.com>
2023-11-01 14:11:13 -0700

    First commit after second commit

---
+++ FavoriteProgrammingLanguage.java
@@ -0,0 +1,11 @@
+/**
+ * FavoriteProgrammingLanguage
+ */
+public class FavoriteProgrammingLanguage {
+    public static void main(String[] args) 
+    {
+        System.out.println("My favoroite language is Java");
+    }
+
+}
+    
