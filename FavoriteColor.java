2bc46f384c9582a53ec6b33711035712cb79a1cc
dwilcox <wilcox.dean93@gmail.com>
2023-11-01 15:08:43 -0700

    adding colors

---
+++ FavoriteColor.java
@@ -0,0 +1,6 @@
+public class FavoriteColor {
+    public static void main(String[] args) {
+        System.out.println("Purple or maybe not that one, but maybe another.");
+        System.out.println("Yeah, actually not purple - it's blue.");
+    }
+}
