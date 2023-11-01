df5b8aa0984d047d4643282f239d15c99ca81511
dwilcox <wilcox.dean93@gmail.com>
2023-11-01 14:08:16 -0700

    Second commit

---
+++ FavoriteFruit.java
@@ -0,0 +1,10 @@
+/**
+ * FavoriteFruit
+ */
+public class FavoriteFruit {
+    public static void main(String[] args) 
+    {
+        System.out.println("This is a test - Adding extra text");
+    }
+
+}
